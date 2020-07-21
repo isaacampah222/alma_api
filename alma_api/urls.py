92d176d5252bb559f7389163956c6927730d23f4
IJAMPAH <ijampah@localhost.localdomain>
2020-07-21 21:14:01 +0000

    added new router

--- alma_api/urls.py
+++ alma_api/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('order',orderlistView,basename='order')
+router.register('ordering',orderlistView,basename='ordering')
 
 urlpatterns = [
     path('viewset/', include(router.urls)),
