74a13575c50dbbf4bf0555e5f2995edc2f239dff
IJAMPAH <ijampah@localhost.localdomain>
2020-07-21 21:01:24 +0000

    checking for changes

--- alma_api/urls.py
+++ alma_api/urls.py
@@ -3,7 +3,7 @@ from .views import orderlistView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('order',orderlistView,basename='order')
+router.register('ordering',orderlistView,basename='ordering')
 
 urlpatterns = [
     path('viewset/', include(router.urls)),
