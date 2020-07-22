a47b41ba62adc9b7372cacabf58b81d5e8269ad3
IJAMPAH <ijampah@localhost.localdomain>
2020-07-22 17:30:19 +0000

    adding a generic view

--- alma_api/urls.py
+++ alma_api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
-from .views import orderlistView
+from .views import orderlistView, genericOrderView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('order',orderlistView,basename='order')
-router.register('ordering',orderlistView,basename='ordering')
 
 urlpatterns = [
     path('viewset/', include(router.urls)),
     # path('order/',orderlistView.as_view())
+    path('generic/view/', genericOrderView.as_view())
 ]
--- alma_api/views.py
+++ alma_api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import status, mixins,viewsets
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 from .serializers import orderListSerializer
 from .models import Order_List
 
@@ -10,5 +10,13 @@ class orderlistView(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.Retri
     queryset = Order_List.objects.all()
 
 
+class genericOrderView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+    serializer_class = orderListSerializer
+    queryset = Order_List.objects.all()
 
+    def get(self,request):
+        return self.list(request)
+
+    def  post(self,request):
+        return self.create(request)
 # Create your views here.
